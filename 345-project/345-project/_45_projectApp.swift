bdaf083a8b3b259bf349931d14eab1ba25573ef3
Matthew Jennings <jenma048@student.otago.ac.nz>
2022-07-13 21:37:43 +1200

    First test

---
+++ 345-project/345-project/_45_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _45_projectApp.swift
+//  345-project
+//
+//  Created by Matthew Jennings on 13/07/22.
+//
+
+import SwiftUI
+
+@main
+struct _45_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
